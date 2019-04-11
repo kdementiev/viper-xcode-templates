da79691092a316088c41bca2f8eca560789437a3
Konstantine Dementiev <kdementiev@reinvently.com>
2019-04-11 18:42:59 +0300

    Add support of Swift 4 & update text format.

--- Architecture/VIPER-Swift.xctemplate/Configuration/Contract/___FILEBASENAME___RouterProtocol.swift
+++ Architecture/VIPER-Swift.xctemplate/Configuration/Contract/___FILEBASENAME___RouterProtocol.swift
@@ -3,11 +3,11 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
+//  ___COPYRIGHT___
 //
 
 import Foundation
 
 protocol ___VARIABLE_productName___RouterProtocol: class {
-    
+
 }
--- Architecture/VIPER-Swift.xctemplate/Configuration/Factory/___FILEBASENAME___ModuleConfigurator.swift
+++ Architecture/VIPER-Swift.xctemplate/Configuration/Factory/___FILEBASENAME___ModuleConfigurator.swift
@@ -3,34 +3,33 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//___COPYRIGHT___
+//  ___COPYRIGHT___
 //
 
 import UIKit
 
 class ___VARIABLE_productName___ModuleConfigurator {
-    
+
     static func configurateModule(view: ___VARIABLE_productName___ViewProtocol?, delegate: ___VARIABLE_productName___ModuleOutputProtocol?)
         -> (view: UIViewController?, module: ___VARIABLE_productName___ModuleInputProtocol?) {
-        
+
         assert(view != nil, "Please, provide view instance.")
-        
+
         let interactor = ___VARIABLE_productName___Interactor()
         let presenter = ___VARIABLE_productName___Presenter()
         let router = ___VARIABLE_productName___Router()
-        
+
         view?.output = presenter
-        
+
         interactor.output = presenter
-        
+
         presenter.interactor = interactor
         presenter.router = router
         presenter.view = view
         presenter.moduleOutput = delegate
-        
+
         router.viewController = view as? UIViewController
-        
+
         return (router.viewController, presenter)
     }
 }
-
